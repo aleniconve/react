6a55e94d3f66a9d7fd14e72fa4df948722cde46f
Alejandro Contreras <contrerasx@gmail.com>
2024-04-12 11:15:02 +0200

    trying syntaxes

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
         <h2>Core Concepts</h2>
         <ul>
           <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
-          <CoreConcept {...CORE_CONCEPTS[1]} />
+          <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image} />
           <CoreConcept {...CORE_CONCEPTS[2]}/>
           <CoreConcept {...CORE_CONCEPTS[3]}/>
         </ul>
